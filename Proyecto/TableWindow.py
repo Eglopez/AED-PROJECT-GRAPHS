086d8e169a38fff71da4b2e94ccf760b7f0a7bd2
Eduardo <eduardolopezlainez2001@gmail.com>
2019-09-23 20:14:13 -0600

    Little changes

--- Proyecto/TableWindow.py
+++ Proyecto/TableWindow.py
@@ -22,11 +22,11 @@ class TableWindow(QtWidgets.QWidget):
         self.table.appendPlainText("-"*57)
         self.table.appendPlainText("\n\n")
         self.table.appendPlainText("-"*57)
-        self.table.appendPlainText("|%sRuta%s|%sPeso%s|"%(" "*12," "*12," "*11," "*11))
+        self.table.appendPlainText("|%sRuta%s|%sPeso%s|"%(" "*11," "*11," "*11," "*11))
         self.table.appendPlainText("-"*57)
         self.parent.roads.sort(key = lambda x:len(x),reverse = True)
         for i in range(len(self.parent.roads)):
-            self.table.appendPlainText('{3}{0:12s}{1:16s}{3}{0:11s}{2:.1f}{0:11s}{3}'.format(" ",",".join(self.parent.roads[i]),self.totalWeigth(self.parent.roads[i]), '|'))#"%s%s%s|%s%s"%(" "*18,," "*7," "*7,))
+            self.table.appendPlainText('{3}{0:11s}{1:15s}{3}{0:8s}{2:7.1f}{0:11s}{3}'.format(" ",",".join(self.parent.roads[i]),self.totalWeigth(self.parent.roads[i]), '|'))#"%s%s%s|%s%s"%(" "*18,," "*7," "*7,))
             self.table.appendPlainText("-"*57)
 
     def totalWeigth(self,array):
@@ -36,7 +36,6 @@ class TableWindow(QtWidgets.QWidget):
         for i in range(1,len(array)):
             key = (current,array[i])
             value = self.parent.edge_labels.get(key)
-            print(key)
             totalWeight = totalWeight + value
             current = array[i]
         return round(totalWeight,2)
